e56acdad5c45978f85557f0819ab4214ece2cc94
lucasbogo <lucasbogo93@gmail.com>
2022-09-11 16:26:46 -0300

    fresh repository

---
+++ app/src/main/java/com/example/activitytimer/ActivitiesContract.java
@@ -0,0 +1,57 @@
+package com.example.activitytimer;
+
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+import static com.example.activitytimer.AppProvider.CONTENT_AUTHORITY;
+import static com.example.activitytimer.AppProvider.CONTENT_AUTHORITY_URI;
+
+
+/**
+ * CONTRACT INTERFACE FOR CONSTANT VALUES
+ * INTERFACE CHAMADA 'CONTRACT' QUE SERVE P/ DECLARAR VALORES CONSTANTES
+ *
+ * OU SEJA, ESSA CLASSE SERVE PARA DECLARAR AS CONSTANTES (NOME_COLUNAS)
+ * QUE SERÃO UTILIZADAS NESTE PROJETO. ASSIM NÃO SERÁ NECESSÁRIO CRIAR INSTÂNCIAS DA CLASSE COLUNA
+ *
+ */
+
+public class ActivitiesContract {
+
+    static final String TABLE_NAME = "activities";
+
+    // Activities fields
+    public static class Columns {
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String ACTIVITIES_NAME = "name";
+        public static final String ACTIVITIES_DESCRIPTION = "description";
+        public static final String ACTIVITIES_SORT_ORDER = "sort_order";
+
+        // Construtor privado para prevenir instanciação
+        private Columns() {
+            /**
+             * Esse construtor não tem nenhum funcionalidade. Porém, como está privado
+             * instâncias da classe coluna não poderão ser criadas...
+             */
+        }
+    }
+
+    /**
+     * Uri que acessa a 'activities' table.
+     */
+    public static final Uri CONTENT_URI = Uri.withAppendedPath(CONTENT_AUTHORITY_URI, TABLE_NAME);
+
+    // Constantes:
+    static final String CONTENT_TYPE_= "vnd.android.cursor.dir/vnd." + CONTENT_AUTHORITY + "." + TABLE_NAME;
+    static final String CONTENT_ITEM_TYPE_= "vnd.android.cursor.dir/vnd." + CONTENT_AUTHORITY + "." + TABLE_NAME;
+
+    static Uri BuildActivityUri(long activitiesId) {
+        return ContentUris.withAppendedId(CONTENT_URI, activitiesId);
+    }
+
+    static long getActivityId(Uri uri) {
+        return ContentUris.parseId(uri);
+    }
+}
---
+++ app/src/main/java/com/example/activitytimer/AppProvider.java
@@ -0,0 +1,149 @@
+package com.example.activitytimer;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.time.Duration;
+
+/**
+ * Criado por Lucas Bogo em 09/09/2022
+ *
+ * 'Provider' para o aplicativo ActivityTimer;
+ *
+ * Essa é a única classe que conhece {@link AppDatabase}
+ *
+ */
+
+public class AppProvider extends ContentProvider {
+
+    // Contantes
+    private static final String TAG = "AppProvider";
+
+    private AppDatabase mOpenHelper;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    static final String CONTENT_AUTHORITY = "com.example.activitytimer.provider";
+    public static final Uri CONTENT_AUTHORITY_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private static final int ACTIVITIES = 100;
+    private static final int ACTIVITIES_ID = 101;
+
+    private static final int TIMINGS = 200;
+    private static final int TIMINGS_ID = 201;
+
+    //private static final int ACTIVITIES_TIMINGS = 300;
+    //private static final int ACTIVITIES_TIMINGS_ID = 301;
+
+    private static final int ACTIVITIES_DURATIONS = 400;
+    private static final int ACTIVITIES_DURATIONS_ID = 401;
+    // Final constantes
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        // Exemplo: content://com.example.activitytimer.provider/Activities
+        matcher.addURI(CONTENT_AUTHORITY, ActivitiesContract.TABLE_NAME, ACTIVITIES);
+
+        // Exemplo: content://com.example.activitytimer.provider/Activities/8
+        matcher.addURI(CONTENT_AUTHORITY, ActivitiesContract.TABLE_NAME + "/#", ACTIVITIES_ID);
+
+       /* matcher.addURI(CONTENT_AUTHORITY, TimingsContract.TABLE_NAME, TIMINGS);
+        matcher.addURI(CONTENT_AUTHORITY, TimingsContract.TABLE_NAME + "/#", TIMINGS_ID);
+
+        matcher.addURI(CONTENT_AUTHORITY, DurationsContract.TABLE_NAME, ACTIVITIES_DURATIONS);
+        matcher.addURI(CONTENT_AUTHORITY, DurationsContract.TABLE_NAME + "/#", ACTIVITIES_DURATIONS_ID);*/
+
+        return matcher;
+
+    }
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = AppDatabase.getInstance(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        Log.d(TAG, "query: chamado com a URI " + uri);
+        final int match = sUriMatcher.match(uri);
+        Log.d(TAG, "query: match é " + match);
+
+        SQLiteQueryBuilder sqLiteQueryBuilder = new SQLiteQueryBuilder();
+
+        switch (match) {
+            case ACTIVITIES:
+                sqLiteQueryBuilder.setTables(ActivitiesContract.TABLE_NAME);
+                break;
+
+            case ACTIVITIES_ID:
+                sqLiteQueryBuilder.setTables(ActivitiesContract.TABLE_NAME);
+                long activitiesId = ActivitiesContract.getActivityId(uri);
+                sqLiteQueryBuilder.appendWhere(ActivitiesContract.Columns._ID + " = " + activitiesId);
+                break;
+
+        /*  case TIMINGS:
+                sqLiteQueryBuilder.setTables(TimingsContract.TABLE_NAME);
+                break;
+
+            case TIMINGS_ID:
+                sqLiteQueryBuilder.setTables(TimingsContract.TABLE_NAME);
+                long timingId = ActivitiesContract.getTimingId(uri);
+                sqLiteQueryBuilder.appendWhere(ActivitiesContract.Columns._ID + " = " + timingId);
+                break;
+
+            case ACTIVITIES_DURATIONS:
+                sqLiteQueryBuilder.setTables(DurationsContract.TABLE_NAME);
+                break;
+
+            case ACTIVITIES_DURATIONS_ID:
+                sqLiteQueryBuilder.setTables(DurationsContract.TABLE_NAME);
+                long activitiesDurationIdgId = ActivitiesContract.activitiesDurationId(uri);
+                sqLiteQueryBuilder.appendWhere(ActivitiesContract.Columns._ID + " = " + activitiesDurationId);
+                break; */
+
+
+            default:
+                throw new IllegalArgumentException("URI desconhecida: " + uri);
+
+        }
+
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        // Passei os parâmetros do 'query method' acima
+        return sqLiteQueryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
