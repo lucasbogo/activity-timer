4733ceebf5b3fb5ba0ed6925ac2b9086393762da
lucasbogo <lucasbogo93@gmail.com>
2022-09-12 14:43:42 -0300

    add_edit linear layout added

--- app/src/main/java/com/example/activitytimer/MainActivity.java
+++ app/src/main/java/com/example/activitytimer/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.activitytimer;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,64 +23,66 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-   // private static final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        String[] projection = { ActivitiesContract.Columns._ID,
-                                ActivitiesContract.Columns.ACTIVITIES_NAME,
-                                ActivitiesContract.Columns.ACTIVITIES_DESCRIPTION,
-                                ActivitiesContract.Columns.ACTIVITIES_SORT_ORDER  };
+        String[] projection = {ActivitiesContract.Columns._ID,
+                ActivitiesContract.Columns.ACTIVITIES_NAME,
+                ActivitiesContract.Columns.ACTIVITIES_DESCRIPTION,
+                ActivitiesContract.Columns.ACTIVITIES_SORT_ORDER};
 
 
-        // ContentResolver contentResolver = getContentResolver();
+        ContentResolver contentResolver = getContentResolver();
 
         // Inserindo dados para teste via ContentValues
-        // ContentValues values = new ContentValues();
+        ContentValues values = new ContentValues();
 
-        /*values.put(ActivitiesContract.Columns.ACTIVITIES_NAME, "Atividade 1");
+        values.put(ActivitiesContract.Columns.ACTIVITIES_NAME, "Atividade 1");
         values.put(ActivitiesContract.Columns.ACTIVITIES_DESCRIPTION, "Descrição 1");
         values.put(ActivitiesContract.Columns.ACTIVITIES_SORT_ORDER, 1);
-        Uri uri = contentResolver.insert(ActivitiesContract.CONTENT_URI, values);*/
+        Uri uri = contentResolver.insert(ActivitiesContract.CONTENT_URI, values);
 
-      /*  Cursor cursor = contentResolver.query(ActivitiesContract.CONTENT_URI,
-                projection,null,null, ActivitiesContract.Columns.ACTIVITIES_NAME);
+        Cursor cursor = contentResolver.query(ActivitiesContract.CONTENT_URI,
+                projection, null, null, ActivitiesContract.Columns.ACTIVITIES_NAME);
 
         // Se o cursor não for igual a nulo:
         if (cursor != null) {
             Log.d(TAG, "onCreate: numeros de linhas:" + cursor.getCount());
             // Enquanto:
-            while(cursor.moveToNext()){
-                for (int i=0; i<cursor.getColumnCount(); i++) {
+            while (cursor.moveToNext()) {
+                for (int i = 0; i < cursor.getColumnCount(); i++) {
                     Log.d(TAG, "onCreate: " + cursor.getColumnName(i) + ": " + cursor.getString(i));
                 }
                 Log.d(TAG, "onCreate: teste");
-            }*/
+            }
+            cursor.close();
+        }
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.menumain_settings) {
+        @Override
+        public boolean onCreateOptionsMenu (Menu menu){
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+    @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
 
-}
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.menumain_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+    }
