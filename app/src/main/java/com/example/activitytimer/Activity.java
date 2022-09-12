cd68297706eff7ec93623df83f0e7ab73985bb4e
lucasbogo <lucasbogo93@gmail.com>
2022-09-12 16:05:16 -0300

    getters and setter for activity class

---
+++ app/src/main/java/com/example/activitytimer/Activity.java
@@ -0,0 +1,39 @@
+package com.example.activitytimer;
+
+import java.io.Serializable;
+
+class Activity implements Serializable {
+    public static final long serialVersionUID = 20220912L;
+
+    private long m_Id;
+    private final String mName;
+    private final String mDescription;
+    private final int mSortOrder;
+
+    public Activity(long id, String name, String description, int sortOrder) {
+        this.m_Id = id;
+        this.mName = name;
+        this.mDescription = description;
+        this.mSortOrder = sortOrder;
+    }
+
+    public long getId() {
+        return m_Id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public int getSortOrder() {
+        return mSortOrder;
+    }
+
+    public void setId(long id) {
+        this.m_Id = id;
+    }
+}
