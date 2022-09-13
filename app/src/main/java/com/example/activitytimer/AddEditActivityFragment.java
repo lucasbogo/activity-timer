4c6149c9d62df21b5f18cda861d05484d84767e2
lucasbogo <lucasbogo93@gmail.com>
2022-09-12 23:08:27 -0300

    view created

--- app/src/main/java/com/example/activitytimer/AddEditActivityFragment.java
+++ app/src/main/java/com/example/activitytimer/AddEditActivityFragment.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +17,14 @@ import androidx.fragment.app.Fragment;
 public class AddEditActivityFragment extends Fragment {
     private static final String TAG = "AddEditActivityFragment";
 
+    public enum FragmentEditMode { EDIT, ADD }
+    private FragmentEditMode mMode;
+
+    private EditText mNameTextView;
+    private EditText mNameDescriptionTextView;
+    private EditText mNameSortOrderTextView;
+    private Button mSaveButton;
+
     public AddEditActivityFragment() {
         Log.d(TAG, "AddEditActivityFragment: constructor called");
     }
@@ -23,6 +33,13 @@ public class AddEditActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: starts");
-        return inflater.inflate(R.layout.fragment_add_edit, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_edit, container, false);
+
+        mNameTextView = (EditText) view.findViewById(R.id.add_edit_name);
+        mNameDescriptionTextView = (EditText) view.findViewById(R.id.add_edit_description);
+        mNameSortOrderTextView = (EditText) view.findViewById(R.id.add_edit_sort_order);
+        mSaveButton = (Button) view.findViewById(R.id.add_edit_save);
+
+        return view;
     }
 }
