4f79b4117820f5b3fb28d0198de2d54964d1d99d
lucasbogo <lucasbogo93@gmail.com>
2022-09-13 00:09:55 -0300

    CRUD ADD

--- app/src/main/java/com/example/activitytimer/AddEditActivityFragment.java
+++ app/src/main/java/com/example/activitytimer/AddEditActivityFragment.java
@@ -1,5 +1,7 @@
 package com.example.activitytimer;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -40,6 +42,75 @@ public class AddEditActivityFragment extends Fragment {
         mNameDescriptionTextView = (EditText) view.findViewById(R.id.add_edit_description);
         mNameSortOrderTextView = (EditText) view.findViewById(R.id.add_edit_sort_order);
         mSaveButton = (Button) view.findViewById(R.id.add_edit_save);
+
+        Bundle arguments = getActivity().getIntent().getExtras(); // Mudarei isso depois
+
+        final Activity activity;
+        if(arguments != null) {
+            Log.d(TAG, "onCreateView: Buscando os detalhes da atividade");
+
+            activity = (Activity) arguments.getSerializable(Activity.class.getSimpleName());
+            if (activity != null) {
+                Log.d(TAG, "onCreateView: Detalhes da atividade achado, editando... ");
+                mNameTextView.setText(activity.getName());
+                mNameDescriptionTextView.setText(activity.getDescription());
+                mNameSortOrderTextView.setText(Integer.toString(activity.getSortOrder()));
+                mMode = FragmentEditMode.EDIT;
+            } else {
+                // No task, so we must be adding a new task, and not editing an  existing one
+                mMode = FragmentEditMode.ADD;
+            }
+        } else {
+            activity = null;
+            Log.d(TAG, "onCreateView: No arguments, adding new record");
+            mMode = FragmentEditMode.ADD;
+        }
+
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Update the database if at least one field has changed.
+                // - There's no need to hit the database unless this has happened.
+                int so;     // to save repeated conversions to int.
+                if(mNameSortOrderTextView.length()>0) {
+                    so = Integer.parseInt(mNameSortOrderTextView.getText().toString());
+                } else {
+                    so = 0;
+                }
+
+                ContentResolver contentResolver = getActivity().getContentResolver();
+                ContentValues values = new ContentValues();
+
+                switch (mMode) {
+                    case EDIT:
+                        if(!mNameTextView.getText().toString().equals(activity.getName())) {
+                            values.put(ActivitiesContract.Columns.ACTIVITIES_NAME, mNameTextView.getText().toString());
+                        }
+                        if(!mNameDescriptionTextView.getText().toString().equals(activity.getDescription())) {
+                            values.put(ActivitiesContract.Columns.ACTIVITIES_DESCRIPTION, mNameDescriptionTextView.getText().toString());
+                        }
+                        if(so != activity.getSortOrder()) {
+                            values.put(ActivitiesContract.Columns.ACTIVITIES_SORT_ORDER, so);
+                        }
+                        if(values.size() != 0) {
+                            Log.d(TAG, "onClick: updating task");
+                            contentResolver.update(ActivitiesContract.BuildActivityUri(activity.getId()), values, null, null);
+                        }
+                        break;
+                    case ADD:
+                        if(mNameTextView.length()>0) {
+                            Log.d(TAG, "onClick: adding new task");
+                            values.put(ActivitiesContract.Columns.ACTIVITIES_NAME, mNameTextView.getText().toString());
+                            values.put(ActivitiesContract.Columns.ACTIVITIES_DESCRIPTION, mNameDescriptionTextView.getText().toString());
+                            values.put(ActivitiesContract.Columns.ACTIVITIES_SORT_ORDER, so);
+                            contentResolver.insert(ActivitiesContract.CONTENT_URI, values);
+                        }
+                        break;
+                }
+                Log.d(TAG, "onClick: Done editing");
+            }
+        });
+        Log.d(TAG, "onCreateView: Exiting...");
 
         return view;
     }
